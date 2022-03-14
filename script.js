8973e135a52247f765a6a417dc751fe4527c318e
Devin Berkani <devinberkani@gmail.com>
2022-03-14 14:52:45 -0400

    make javascript look better

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-let border = document.getElementById('border');
+let about = document.getElementById('aboutId');
+let projects = document.getElementById('projectsId');
+let skills = document.getElementById('skillsId');
+let contact = document.getElementById('contactId');
 
-let bigBorder = () => {
-    border.style.border = '3px solid #D3D3D3';
+let aboutUnderline = () => {
+    about.style.textDecoration = 'underline';
 };
 
-let yesBorder = () => {
-    border.style.border = '1px solid #D3D3D3';
+let noAboutUnderline = () => {
+    about.style.textDecoration = '';
+};
+
+let projectsUnderline = () => {
+    projects.style.textDecoration = 'underline';
+};
+
+let noProjectsUnderline = () => {
+    projects.style.textDecoration = '';
+};
+
+let skillsUnderline = () => {
+    skills.style.textDecoration = 'underline';
+};
+
+let noSkillsUnderline = () => {
+    skills.style.textDecoration = '';
+};
+
+let contactUnderline = () => {
+    contact.style.textDecoration = 'underline';
+};
+
+let noContactUnderline = () => {
+    contact.style.textDecoration = '';
 };
 
 
-border.addEventListener('mouseover', bigBorder);
-border.addEventListener('mouseout', yesBorder);
+skills.style.textDecoration = 'underline';
+contact.style.textDecoration = 'underline';
+
+about.addEventListener('mouseover', aboutUnderline);
+about.addEventListener('mouseout', noAboutUnderline);
+
+projects.addEventListener('mouseover', projectsUnderline);
+projects.addEventListener('mouseout', noProjectsUnderline);
+
+skills.addEventListener('mouseover', skillsUnderline);
+skills.addEventListener('mouseout', noSkillsUnderline);
+
+contact.addEventListener('mouseover', contactUnderline);
+contact.addEventListener('mouseout', noContactUnderline);
